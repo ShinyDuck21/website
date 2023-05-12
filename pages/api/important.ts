56e284256f36bff439dad134bb7f623fe440d1b9
ShinyDuck21 <84870282+ShinyDuck21@users.noreply.github.com>
2023-05-11 21:17:18 -0500

    Content Yes

---
+++ pages/api/important.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    name: string
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    res.status(200).json({
+        about: 'This contains an important youtube link that is needed for this website to function properly. It is of a cute puppy',
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D'
+    })
+}
